66dfe85339c5c6ca2ea9b016743fe928bf6f0dd7
guillermocalderon2021 <77698223+guillermocalderon2021@users.noreply.github.com>
2025-08-27 18:47:30 -0600

    Add files via upload

---
+++ tutoria5/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Tutoria5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.txtJ = new System.Windows.Forms.TextBox();
+            this.txtI = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lst1 = new System.Windows.Forms.ListBox();
+            this.lst2 = new System.Windows.Forms.ListBox();
+            this.lst3 = new System.Windows.Forms.ListBox();
+            this.lst4 = new System.Windows.Forms.ListBox();
+            this.lsi5 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnProcesar);
+            this.groupBox1.Controls.Add(this.txtJ);
+            this.groupBox1.Controls.Add(this.txtI);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 202);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(75, 142);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 4;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // txtJ
+            // 
+            this.txtJ.Location = new System.Drawing.Point(65, 89);
+            this.txtJ.Name = "txtJ";
+            this.txtJ.Size = new System.Drawing.Size(100, 22);
+            this.txtJ.TabIndex = 3;
+            // 
+            // txtI
+            // 
+            this.txtI.Location = new System.Drawing.Point(65, 42);
+            this.txtI.Name = "txtI";
+            this.txtI.Size = new System.Drawing.Size(100, 22);
+            this.txtI.TabIndex = 2;
+            this.txtI.TextChanged += new System.EventHandler(this.txtI_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "j:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "i:";
+            // 
+            // lst1
+            // 
+            this.lst1.FormattingEnabled = true;
+            this.lst1.ItemHeight = 16;
+            this.lst1.Location = new System.Drawing.Point(323, 75);
+            this.lst1.Name = "lst1";
+            this.lst1.Size = new System.Drawing.Size(76, 132);
+            this.lst1.TabIndex = 1;
+            // 
+            // lst2
+            // 
+            this.lst2.FormattingEnabled = true;
+            this.lst2.ItemHeight = 16;
+            this.lst2.Location = new System.Drawing.Point(429, 75);
+            this.lst2.Name = "lst2";
+            this.lst2.Size = new System.Drawing.Size(76, 132);
+            this.lst2.TabIndex = 2;
+            // 
+            // lst3
+            // 
+            this.lst3.FormattingEnabled = true;
+            this.lst3.ItemHeight = 16;
+            this.lst3.Location = new System.Drawing.Point(534, 75);
+            this.lst3.Name = "lst3";
+            this.lst3.Size = new System.Drawing.Size(76, 132);
+            this.lst3.TabIndex = 3;
+            // 
+            // lst4
+            // 
+            this.lst4.FormattingEnabled = true;
+            this.lst4.ItemHeight = 16;
+            this.lst4.Location = new System.Drawing.Point(629, 75);
+            this.lst4.Name = "lst4";
+            this.lst4.Size = new System.Drawing.Size(76, 132);
+            this.lst4.TabIndex = 4;
+            // 
+            // lsi5
+            // 
+            this.lsi5.FormattingEnabled = true;
+            this.lsi5.ItemHeight = 16;
+            this.lsi5.Location = new System.Drawing.Point(732, 75);
+            this.lsi5.Name = "lsi5";
+            this.lsi5.Size = new System.Drawing.Size(76, 132);
+            this.lsi5.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 313);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 399);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lsi5);
+            this.Controls.Add(this.lst4);
+            this.Controls.Add(this.lst3);
+            this.Controls.Add(this.lst2);
+            this.Controls.Add(this.lst1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtJ;
+        private System.Windows.Forms.ListBox lst1;
+        private System.Windows.Forms.ListBox lst2;
+        private System.Windows.Forms.ListBox lst3;
+        private System.Windows.Forms.ListBox lst4;
+        private System.Windows.Forms.ListBox lsi5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ tutoria5/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutoria5
+{
+    public partial class Form1 : Form
+    {
+        static Stack<int> p1 = new Stack<int>();
+        static Stack<int> p2 = new Stack<int>();
+        static Stack<int> p3 = new Stack<int>();
+        static Stack<int> p4 = new Stack<int>();
+        static Stack<int> p5 = new Stack<int>();
+
+        Stack<int>[] pilas = { p1, p2, p3, p4, p5 };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtI_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = int.Parse(txtI.Text);
+                int j = int.Parse(txtJ.Text);
+                if (Math.Abs(i) > 5)
+                {
+                    MessageBox.Show("El valor absoluto de i debe ser menor o igual a 5");
+                    return;
+                }
+                if (i > 0) // Insertar j en la pila i
+                {
+                    pilas[i - 1].Push(j);
+                }
+                else if (i < 0)
+                {
+                    if(pilas[Math.Abs(i) - 1].Count == 0)
+                    {
+                        MessageBox.Show("La pila esta vacia");
+                    }
+                    else
+                    {
+                        // Eliminar el tope de la pila
+                        pilas[Math.Abs(i) - 1].Pop();
+                    }
+                        
+                }
+                else
+                {
+                    MessageBox.Show("Fin del proceso de ingreso");
+                }
+                MostrarPilas();
+                txtI.Clear();
+                txtJ.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void MostrarPilas()
+        {
+            ListBox[] listboxes = { lst1, lst2, lst3, lst4, lsi5 };
+            for (int i = 0; i < listboxes.Length; i++)
+            {
+                listboxes[i].Items.Clear();
+                foreach (int n in pilas[i])
+                {
+                    listboxes[i].Items.Add(n);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text); //285
+            Pila pila = new Pila();
+            int temp=num; // temp = 2 , digito= 2
+            while (temp > 0)
+            {
+                int digito = temp % 10; // Tomando el ultimo digito
+                pila.Push(digito);
+                temp = temp / 10; // Quitando el ultimo digito
+            }
+            // pila: 
+
+            // Reconstruir el numero inverso
+            int inverso = 0;
+            int multiplicador = 1; // multiplicador =1000
+            while (pila.Count > 0)
+            {
+                int digito= pila.Pop(); // digito = 5
+                inverso+=  digito*multiplicador; // inverso = 582
+                multiplicador *= 10;
+            }
+            if (inverso == num)
+            {
+                MessageBox.Show($"{num} es capicua");
+            }
+            else
+            {
+                MessageBox.Show($"{num} NO es capicua");
+            }
+        }
+    }
+}
