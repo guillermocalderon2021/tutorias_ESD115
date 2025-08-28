6af662299e1eb2e71c2b4dc72f1ad1148cc349b8
guillermocalderon2021 <77698223+guillermocalderon2021@users.noreply.github.com>
2025-08-27 18:46:31 -0600

    Add files via upload

---
+++ tutoria4/Nodo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutoria5
+{
+    internal class Nodo
+    {
+        public int Item { get; set; }
+        public Nodo Next { get; set; }
+        public Nodo(int item)
+        {
+            this.Item = item;
+            this.Next = null;
+        }
+        public Nodo(int item, Nodo next)
+        {
+            this.Item = item;
+            this.Next = next;
+        }
+    }
+}
---
+++ tutoria4/Pila.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutoria5
+{
+    internal class Pila
+    {
+        private Nodo tope;
+        private int count;
+        public Pila()
+        {
+            tope = null;
+            count = 0;
+        }
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+        public void Push(int item)
+        {
+            if (this.tope == null)
+            {
+                //la pila esta vacia
+                this.tope = new Nodo(item);
+            }
+            else
+            {
+                Nodo nuevoNodo = new Nodo(item, this.tope); 
+                this.tope = nuevoNodo;
+            }
+            this.count++;
+        }
+        public int Peek()
+        {
+            if (this.count == 0)
+            {
+                // pila vacia
+                throw new InvalidOperationException("La pila está vacía, no hay elementos");
+            }
+            return this.tope.Item;
+        }
+
+        public int Pop()
+        {
+            if (this.count == 0)
+            {
+                //pila vacia
+                throw new InvalidOperationException("La pila esta vacia. No hay elementos");
+            }
+            int resultado = this.tope.Item;
+            this.tope = this.tope.Next;
+            this.count--;
+            return resultado;
+        }
+        public void inverso()
+        {
+            int p;
+            Pila paux = new Pila();
+            if (this.count == 0)
+            {
+                // pila vacia
+                throw new InvalidOperationException("La pila está vacía, no hay elementos");
+            }
+            while (this.count > 0)
+            {
+                p = Pop();
+                paux.Push(p);
+                Console.WriteLine("numero sacado de pila: " + p + "\n");
+            }
+        }
+    }
+}
